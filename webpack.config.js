59384dce23f349380c72e25a4cea9ff0916b2091
unknown <h-abe@s2-pc-n025.ocean.s2soft.co.jp>
2015-12-21 14:32:43 +0900

    [modify] use webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: "./src/js/App.js",
+  output: {
+    path: "./src",
+    filename: "bundle.js"
+  },
+  /* ソースマップを出力させる場合は以下を追加 */
+  devtool: "inline-source-map",
+  module: {
+    loaders: [
+        {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", query: { presets: [ 'es2015' ] }},
+        {test: /\.html$/, exclude: /node_modules/, loader: "html-loader"}
+    ]
+  }
+};
