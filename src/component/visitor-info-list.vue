279f163101a63b57e10c057aafa7125348b5c840
AbeHaruhiko <h-abe@ocean.s2soft.co.jp>
2015-12-24 15:21:59 +0900

    [modify] fluxで子コンポーネントへstoreを渡せた

--- src/component/visitor-info-list.vue
+++ src/component/visitor-info-list.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="list" class="pure-u-1">
-    <visitor-info-list-item v-for="visitorInfo in visitorInfoList" :visitor-info="visitorInfo"></visitor-info-list-item>
+    <visitor-info-list-item v-for="visitorInfo in privateVisitorInfoList" :visitor-info="visitorInfo"></visitor-info-list-item>
 </div>
 </template>
 
@@ -10,14 +10,15 @@ import VisitorInfoListItem from '../component/visitor-info-list-item.vue'
 
 export default {
     props: {
-        visitorInfoList: [],
+        privateVisitorInfoList: [],
+        store: {}
     },
     components: {
         VisitorInfoListItem
     },
     created: function() {
-        this.visitorInfoList = [];
-        console.log(store)
+        this.privateVisitorInfoList = this.store.getVisitorInfoList();
+        console.log(this.store)
     }
 }
 </script>
--- src/js/App.js
+++ src/js/App.js
@@ -14,47 +14,45 @@ import EventEmitter from "./flux/EventEmitter"
 
 Vue.config.debug = true
 
-// Application entry point
-window.onload = () => {
+// init Parse
+Parse.initialize("Ikzt3vnq6LwIKSb4WDP8RkOcUW3wRlsQuLUlrrFN", "mQTMG00TR3azol0UmAT6IIUH0uWFtppRDqDjNS5h")
 
-  // init Parse
-  Parse.initialize("Ikzt3vnq6LwIKSb4WDP8RkOcUW3wRlsQuLUlrrFN", "mQTMG00TR3azol0UmAT6IIUH0uWFtppRDqDjNS5h")
+// コンポーネント登録
+Vue.component('visitor-info-list', VisitorInfoList)
+Vue.component('visitor-info', VisitorInfo)
 
-  // コンポーネント登録
-  Vue.component('visitor-info-list', VisitorInfoList)
-  Vue.component('visitor-info', VisitorInfo)
+var dispatcher = new EventEmitter();
+var action = new ActionCreator(dispatcher);
+var store = new Store(dispatcher);
 
-  var dispatcher = new EventEmitter();
-  var action = new ActionCreator(dispatcher);
-  var store = new Store(dispatcher);
-
-  // root インスタンスを作成する
-  var vm = new Vue({
+// root インスタンスを作成する
+var vm = new Vue({
     el: '#layout',
     data: {
-      visitorInfoList: []
+        visitorInfoList: [],     // storeにも置くので冗長だけど・・・
+        store: store
     },
     created: function() { // ここはアローファンクションにするとthisがvmを指さなくなる。
 
-      this.visitorInfoList = store.getVisitorInfoList()
-      store.on("CHANGE", () => {
-        this._onChange()
-      })
-
-      var query = new Parse.Query('VisitorInfo')
-      query.descending('createdAt')
-      query.include('member')
-
-      query.find()
-      .then(function(results) {
-        _.forEach(results, function(result) {
-            vm.visitorInfoList.push(result.toJSON())
+        this.visitorInfoList = store.getVisitorInfoList()
+        store.on("CHANGE", () => {
+            this._onChange()
         })
-        action.setVisitorInfoList(vm.visitorInfoList)
-      },
-      function(error) {
-        console.error(error)
-      });
+
+        var query = new Parse.Query('VisitorInfo')
+        query.descending('createdAt')
+        query.include('member')
+
+        query.find()
+        .then((results) => {    // arrowにしないとthisがvmを指さない
+            _.forEach(results, (result) => {
+                this.visitorInfoList.push(result.toJSON())
+            })
+            action.setVisitorInfoList(this.visitorInfoList)
+        },
+        function(error) {
+            console.error(error)
+        });
 
     },
     events: {
@@ -62,22 +60,20 @@ window.onload = () => {
             // 親（このコンポーネントのvisitorInfoList）はisSelectedは持っていない。
             // よって子のプロパティを変更する。（後で変えるかも）
             _.forEach(this.$children, function(visitorInfo) {
-                visitorInfo.isSelected = false;
+                this.visitorInfo.isSelected = false;
             })
         }
     },
     methods: {
-      _onChange (){
-        console.trace()
-        this.count = store.getCount()
-      }
+        _onChange (){
+            console.trace()
+            this.count = store.getCount()
+            this.visitorInfoList = store.getVisitorInfoList();
+        }
     }
-  })
+})
 
 
 
-  const date = Util.formatDate();
-  console.log( '[' + date + '] Application was launched.' );
-
-
-};
+const date = Util.formatDate();
+console.log( '[' + date + '] Application was launched.' );
