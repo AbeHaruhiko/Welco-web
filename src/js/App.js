cd94ef6a97d9494d42e86caddffa7a95689e4edb
AbeHaruhiko <caliconography@gmail.com>
2016-01-07 11:21:02 +0900

    [add] routerで表示一応できた。propsはrouter-viewタグにセットできる

--- src/js/App.js
+++ src/js/App.js
@@ -8,28 +8,50 @@ import _ from 'lodash'
 
 import VisitorInfoContainer from '../component/visitor-info-container.vue'
 
-import Store from './Store.js'
+import store from './Store.js'
 
 Vue.config.debug = true
 
 // init Parse
 Parse.initialize("Ikzt3vnq6LwIKSb4WDP8RkOcUW3wRlsQuLUlrrFN", "mQTMG00TR3azol0UmAT6IIUH0uWFtppRDqDjNS5h")
 
-// コンポーネント登録
-Vue.component('visitor-info-container', VisitorInfoContainer)
-
-var store = Store
-
 // routerプラグインインストール
 Vue.use(VueRouter)
 
 // router インスタンスを作成。
-var router = new VueRouter()
+var router = new VueRouter({
+    history: true
+})
 
-// いくつかの routes を定義します
+
+var Foo = Vue.extend({
+    template: '<p>This is foo!</p>'
+})
+
+var Bar = Vue.extend({
+    template: '<p>This is bar!</p>'
+})
+
+
+// routes を定義します
 router.map({
     '/visitorinfo': {
+        data: function() {
+            return { store: store }
+        },
         component: VisitorInfoContainer
+    },
+    '/foo': {
+        // data: function() {
+        //     return { store: store }
+        // },
+        component: Foo
+    },
+    '/bar': {
+        // data: function() {
+        //     return { store: store }
+        // },
+        component: Bar
     }
 })
 
@@ -39,9 +61,14 @@ var App = Vue.extend({
     data: function() {
         return { store: store }
     }
+    // components: {
+    //     'visitor-info-container': VisitorInfoContainer
+    // },
+    // template: '<router-view></router-view>'
 })
 
-router.start(App, '#content')
+// 第二引数のelementをAppでリプレイスする。
+router.start(App, 'app')
 
 
 const date = Util.formatDate();
