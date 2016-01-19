4eb1788792720d083f3a882457b009ba9a199cab
AbeHaruhiko <caliconography@gmail.com>
2016-01-20 00:43:07 +0900

    [modify] 不要コード削除

--- src/component/visitor-info-container.vue
+++ src/component/visitor-info-container.vue
@@ -63,11 +63,8 @@
 import MemberListDd from '../component/member-list-dd.vue'
 import VisitorInfoList from '../component/visitor-info-list.vue'
 import VisitorInfoItem from '../component/visitor-info-item.vue'
-// import { findParseVisitorInfoList } from '../component/parse-visitor-info.js'
 
 import { menu } from '../js/menus.js'
-
-// import Store from '../js/Store.js'
 
 export default {
 
@@ -75,43 +72,15 @@ export default {
         return '#layout'
     },
     props: {
-        // privateVisitorInfoList: [],     // storeにも置くので冗長だけど・・・
         store: {}
     },
     created: function() { // ここはアローファンクションにするとthisがvmを指さなくなる。
-
-        // TODO: ここでなく、visitor-info-list.vueに移動した方がいい？
-
-        // this.visitorInfoList = store.getVisitorInfoList()
-
-        // var query = new Parse.Query('VisitorInfo')
-        // query.descending('createdAt')
-        // query.include('member')
-        //
-        // query.find()
-        // findParseVisitorInfoList()
-        // .then((results) => {    // arrowにしないとthisがvmを指さない
-        //     _.forEach(results, (result) => {
-        //         this.store.state.visitorInfoList.push(result.toJSON())
-        //     })
-        //     // store.visitorInfoList = this.privateVisitorInfoList;
-        // },
-        // function(error) {
-        //     console.error(error)
-        // });
     },
     ready: function() {
         // drop down menus
         menu(window, document)
     },
     events: {
-        // 'clearSelection': function() {
-        //     // 親（このコンポーネントのvisitorInfoList）はisSelectedは持っていない。
-        //     // よって子のプロパティを変更する。（後で変えるかも）
-        //     _.forEach(this.$children, function(visitorInfo) {
-        //         visitorInfo.isSelected = false;
-        //     })
-        // }
     },
     methods: {
     },
